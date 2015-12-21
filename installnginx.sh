16fdf2dfb1c81d9537275374795ee0f63cf8967b
bill-cooper <william.l.cooper1@gmail.com>
2015-12-20 19:01:04 -0600

    Create installnginx.sh

---
+++ installnginx.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo ">>> Installing Nginx"
+
+# Add repo for latest stable nginx
+sudo add-apt-repository -y ppa:nginx/stable
+
+# Update Again
+sudo apt-get update
+
+# Install Nginx
+# -qq implies -y --force-yes
+sudo apt-get install -qq nginx
+
+# place nginx config file
+
+sudo service nginx restart
