a68794c29f3f80ebfc68db37cc24b1ffe4b71f3b
William Cooper <william.l.cooper1@gmail.com>
2016-02-26 04:43:25 -0600

    adding init script

---
+++ init.sh
@@ -0,0 +1,9 @@
+
+sudo apt-get --yes --force-yes update
+
+# install git
+apt-get --yes --force-yes install git
+
+# clone nginx config repo
+git clone https://github.com/cnq/routingserver-config.git /opt/config
+
